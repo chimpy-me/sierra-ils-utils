bec836bb05006afe9aab22f4abdffe31976ca102
Ray Voelker <ray.voelker@gmail.com>
2025-02-03 14:20:30 -0500

    testing get_max_record_id

--- sierra_ils_utils/__init__.py
+++ sierra_ils_utils/__init__.py
@@ -1,7 +1,13 @@
 from .sierra_rest_client import SierraRESTClient
 from .sierra_datetime import SierraDateTime, SierraDate
+from .utils import get_max_record_id
 
 # Create an alias
 SierraAPI = SierraRESTClient
 
-__all__ = ["SierraRESTClient"]
+__all__ = [
+    "SierraRESTClient",
+    "SierraDateTime",
+    "SierraDate",
+    "get_max_record_id"
+]
